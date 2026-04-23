f6bec48760927b823c2f11f93af2686be16a0375
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-23 22:36:17 +0200

    Implement file processing and speed output
    Added file processing functionality with speed reporting.

--- examples/file_process.py
+++ examples/file_process.py
@@ -4,6 +4,7 @@
 from PyNLPIR import *
 import os
 
+
 if __name__ == '__main__':
 
     init_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
@@ -12,12 +13,15 @@ if __name__ == '__main__':
     else:
         raise 'NLPIR initialization fail.'
 
+    
     source_file = './resources/text.txt'
     target_file = './resources/text.txt.out'
     speed = file_process(source_file, target_file, True)
+    
     if speed:
         print 'Processing speed: %d words/s' % speed
     else:
         print 'Processing failed.'
+        
 
     exit()
