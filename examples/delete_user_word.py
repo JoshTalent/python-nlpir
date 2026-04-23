8acd6871fedc4f9eafaf91f6a3037bd3de6d195b
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-23 22:31:01 +0200

    Refactor delete_user_word.py for cleaner code
    Removed unnecessary whitespace and cleaned up the code structure.

--- examples/delete_user_word.py
+++ examples/delete_user_word.py
@@ -4,16 +4,13 @@
 from PyNLPIR import *
 import os
 
-
 if __name__ == '__main__':
-
     
     init_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
     if init(init_dir, Constants.CodeType.UTF8_CODE):
         print 'NLPIR initialization succeed.'
     else:
         raise 'NLPIR initialization fail.'
-
     
     word = '爱思客\tn'
     succeed = delete_user_word(word)
@@ -21,7 +18,6 @@ if __name__ == '__main__':
         print 'Delete (%s) from user dictionary successfully.' % word
     else:
         print '(%s) not exist in the user dictionary.' % word
-
     
     add_user_word(word)
     succeed = delete_user_word(word)
