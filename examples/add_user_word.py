f5899c041f62526df11848167da98f40df3b699f
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-27 22:14:40 +0200

    Update add_user_word.py

--- examples/add_user_word.py
+++ examples/add_user_word.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 #encoding: utf8
 
+
 from PyNLPIR import *
 import os
 
 if __name__ == '__main__':
+
     
     init_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
     if init(init_dir, Constants.CodeType.UTF8_CODE):
