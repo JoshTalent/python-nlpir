87bd0a90b830bbb1b6099cbf16f5f96ff159dda4
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-27 22:14:23 +0200

    Update add_user_word.py

--- examples/add_user_word.py
+++ examples/add_user_word.py
@@ -4,7 +4,6 @@
 from PyNLPIR import *
 import os
 
-
 if __name__ == '__main__':
     
     init_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
@@ -22,3 +21,5 @@ if __name__ == '__main__':
 
     exit()
     
+
+
