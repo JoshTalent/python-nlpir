f53e11c88710bd06083284104c354a5e6164aaf2
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-09-07 20:24:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+
 '''
 setup.py file for NLPIR
 '''
+
 from distutils.core import setup, Extension
  
 NLPIR_module = Extension('_NLPIR',sources=['NLPIR_wrap.cxx'], libraries = ['NLPIR'])
-
 
 setup(name = 'NLPIR',
     version = '2.0',
@@ -13,3 +14,4 @@ setup(name = 'NLPIR',
     ext_modules = [NLPIR_module],
     py_modules = ['NLPIR', 'PyNLPIR'],
     )
+
